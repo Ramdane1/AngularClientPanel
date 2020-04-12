60d7d52df39eb1fda7ec78a5682c590f0eed0b27
Ramdane1 <ramdanemohammedyasser@gmail.com>
2020-04-12 14:48:50 +0200

    Routage and Navbar 124

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { SettingsClientComponent } from './components/settings-client/settings-client.component';
+import { DetailsClientComponent } from './components/details-client/details-client.component';
+import { EditClientComponent } from './components/edit-client/edit-client.component';
+import { AddClientComponent } from './components/add-client/add-client.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "" , component: DashboardComponent },
+  {path: "login", component: LoginComponent},
+  {path: "register", component: RegisterComponent},
+  {path: "client/add", component: AddClientComponent},
+  {path: "client/edit/:id", component: EditClientComponent},
+  {path: "client/:id", component: DetailsClientComponent},
+  {path: "settings", component: SettingsClientComponent},
+  //{path: "**", component: DashboardComponent}
+  {path: "**", component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
