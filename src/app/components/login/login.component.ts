d69270e4f1b4e6c1145732d6f8f0e0203294f7e1
Yasser Ramdane <m.y.ramdane12@gmail.com>
2020-04-24 15:39:39 +0200

    AUTHENTICATION WITH EMAIL AND PASSWORD - 134

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthClientService } from 'src/app/services/auth-client.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  email: string;
+  password: string;
+  constructor(private authService: AuthClientService,
+              private flashMessage: FlashMessagesService,
+              private route: Router) { }
 
   ngOnInit() {
+    this.authService.getAuth().subscribe(auth => {
+      if(auth){
+       return this.route.navigate(['/']);
+      }
+    })
   }
 
+
+  onLogin(){
+    this.authService.login(this.email, this.password)
+    .then(auth => {
+      if(auth){
+        this.flashMessage.show('You are logged succesfully',{
+          cssClass: "alert alert-success", timeout: 5000
+        })
+
+        this.route.navigate(['/']);
+      }
+    })
+    .catch(error => {
+      this.flashMessage.show(error.message,{
+        cssClass: "alert alert-danger", timeout: 10000
+      })
+    })
+  }
 }
--- src/app/services/auth-client.service.ts
+++ src/app/services/auth-client.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import {AngularFireAuth} from 'angularfire2/auth';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthClientService {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) { }
+
+  login(email: string, password: string){
+    return new Promise((resolve, reject) => {
+      this.afAuth.auth.signInWithEmailAndPassword(email,password)
+      .then((userData) => resolve(userData), (error) => reject(error))
+    })
+  }
+
+  getAuth(){
+    return this.afAuth.authState.pipe(map(auth => auth));
+  }
 }
