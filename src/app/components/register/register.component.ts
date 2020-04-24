e87b20c5c0e4fbb21a363b7dac059f0eb224ea71
Yasser Ramdane <m.y.ramdane12@gmail.com>
2020-04-24 20:52:01 +0200

    REGISTER ACCOUNTIN FIREBASE WITH ANGULAR - 138

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthClientService } from 'src/app/services/auth-client.service';
+import { Router } from '@angular/router';
+import { auth } from 'firebase';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  email: string;
+  password: string;
+
+  constructor(private authClient: AuthClientService,
+              private route: Router,
+              private flashMessage: FlashMessagesService) { }
 
   ngOnInit() {
+  }
+
+  onRegister(){
+    this.authClient.register(this.email, this.password)
+    .then(register => {
+      if(register){
+        this.flashMessage.show('Congratulation, You are logged', {
+          cssClass: 'alert alert-success', timeout: 5000
+        })
+        this.route.navigate(['/']);
+      }
+    })
+    .catch(error => {
+      this.flashMessage.show(error.message, {
+        cssClass: 'alert alert-danger', timeout: 10000
+      })
+    })
   }
 
 }
--- src/app/services/auth-client.service.ts
+++ src/app/services/auth-client.service.ts
@@ -17,6 +17,13 @@ export class AuthClientService {
     })
   }
 
+  register(email: string, password: string){
+    return new Promise((resolve, reject) => {
+      this.afAuth.auth.createUserWithEmailAndPassword(email,password)
+      .then((userData) => resolve(userData), (error) => reject(error))
+    })
+  }
+
   loginWithGoogle(){
     return new Promise((resolve, reject) => {
       this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
@@ -31,4 +38,6 @@ export class AuthClientService {
   logOut(){
     this.afAuth.auth.signOut();
   }
+
+
 }
