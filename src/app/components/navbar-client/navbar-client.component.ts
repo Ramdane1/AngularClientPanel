42e44581272687d3bcbcb9679b89ab3c00936af7
Yasser Ramdane <m.y.ramdane12@gmail.com>
2020-04-24 20:09:53 +0200

    Logout - 136

--- src/app/components/navbar-client/navbar-client.component.ts
+++ src/app/components/navbar-client/navbar-client.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthClientService } from 'src/app/services/auth-client.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar-client',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarClientComponent implements OnInit {
 
-  constructor() { }
+  isLogedIn: boolean = false;
+  userLogedIn: string;
+  constructor(private authService: AuthClientService,
+              private flashMessage: FlashMessagesService,
+              private route: Router) { }
 
   ngOnInit() {
+    this.authService.getAuth().subscribe(auth => {
+      if(auth){
+        this.isLogedIn = true;
+        this.userLogedIn = auth.email;
+      }
+      else{
+        this.isLogedIn = false;
+      }
+    })
   }
 
+  onLogOut(){
+    this.authService.logOut();
+    this.isLogedIn = false;
+   return this.route.navigate(['/login']);
+
+  }
 }
--- src/app/services/auth-client.service.ts
+++ src/app/services/auth-client.service.ts
@@ -27,4 +27,8 @@ export class AuthClientService {
   getAuth(){
     return this.afAuth.authState.pipe(map(auth => auth));
   }
+
+  logOut(){
+    this.afAuth.auth.signOut();
+  }
 }
